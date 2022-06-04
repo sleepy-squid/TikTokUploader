44c6bc5a87e0418aa7a2002213cf075565e33d94
DenverW03 <d.whiteley@lancaster.ac.uk>
2022-06-04 18:52:28 +0100

    Successfully logs in with hex edited chromedriver to bypass detection

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 import argparse
 import os
 import time
 
 class TikTokUploader:
     def __init__(self):
-        path = Service(r"C:\Users\Denver Whiteley\Documents\PythonProjects\TikTokUploader\chromedriver.exe")
+        path = Service(r"C:\Users\Denver Whiteley\Documents\PythonProjects\TikTokUploader\chromedrivernew.exe")
         self.driver = webdriver.Chrome(service=path)
         self.executor_url = self.driver.command_executor._url
         self.session_id = self.driver.session_id
+        self.username = "subredditreadings"
+        self.password = "*321Cheeseburger123*"
 
     def upload(self, viewSetting, videoPath, caption):
         #opens browser to tiktok homepage
@@ -23,6 +24,17 @@ class TikTokUploader:
         #navigates to the login page
         self.driver.get("https://www.tiktok.com/login/phone-or-email/email")
         #filling in the login form
+        userfield = self.driver.find_element(By.CSS_SELECTOR,"#loginContainer > div.tiktok-xabtqf-DivLoginContainer.exd0a430 > form > div.tiktok-11aoaap-DivInputContainer.e93gz7m0 > input")
+        userfield.click()
+        userfield.send_keys(self.username)
+        time.sleep(2)
+        passfield = self.driver.find_element(By.CSS_SELECTOR, "#loginContainer > div.tiktok-xabtqf-DivLoginContainer.exd0a430 > form > div.tiktok-15iauzg-DivContainer.ewblsjs0 > div > input")
+        passfield.click()
+        passfield.send_keys(self.password)
+        time.sleep(0.5)
+        loginbutton = self.driver.find_element(By.CSS_SELECTOR, "#loginContainer > div.tiktok-xabtqf-DivLoginContainer.exd0a430 > form > button")
+        loginbutton.click();
+        time.sleep(5)
         
 
 def main():
